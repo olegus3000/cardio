b8783959cccb990f4d1daca93686c46706bcabd7
olegus3000 <oleg.dukhnytskyi@sugarcrm.com>
2024-02-13 00:45:41 +0200

    add DataTables

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,41 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Card;
 use App\Entity\Settings;
 use App\Form\ListType;
 use App\Repository\CardRepository;
+use Doctrine\ORM\QueryBuilder;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\DataTable;
+use Omines\DataTablesBundle\DataTableFactory;
+use Omines\DataTablesBundle\Adapter\ArrayAdapter;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class ListController extends AbstractController
 {
+
+    private DataTableFactory $factory;
+
+    public function __construct(DataTableFactory $factory) {
+        $this->factory = $factory;
+    }
+
+    private function createDataTable(array $options = []): DataTable
+    {
+        return $this->factory->create($options);
+    }
+
     #[Route('/list', name: 'app_list')]
-    public function list(Request $request, CardRepository $cardRepository): Response
+    public function list(Request $request): Response
     {
         if (empty($this->getUser())) {
             return $this->redirectToRoute('app_login');
         }
 
-        $settings = new Settings();
+        $table = $this->createDataTable()
+            ->add('sideA', TextColumn::class, ['label' => 'Side A'])
+            ->add('sideB', TextColumn::class, ['label' => 'Side B'])
+            ->createAdapter(ORMAdapter::class, [
+                'entity' => Card::class,
+                'query' => function (QueryBuilder $builder) {
+                    $builder
+                        ->select('c')
+                        ->from(Card::class, 'c');
+                },
+            ])
+            ->handleRequest($request);
 
-        $cards = $cardRepository->findAll();
-
-        foreach ($cards as $card) {
-            $settings->getCards()->add($card);
-        }
-
-        $form = $this->createForm(ListType::class, $settings);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // ... do your form processing, like saving the Task and Tag entities
+        if ($table->isCallback()) {
+            return $table->getResponse();
         }
 
         return $this->render('list/list.html.twig', [
-            'form' => $form,
+            'datatable' => $table
         ]);
 
     }
